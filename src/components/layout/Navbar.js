b3d37ff18919a5b7bd32792d4fbff9ba5d83565e
Emre <emrekurtt1655@gmail.com>
2021-07-22 12:37:51 +0200

    feat: add navbar

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link, NavLink  }  from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav className="green">
+            <div className="nav-wrapper">
+                <Link to="/" className="brand-logo">EverNote</Link>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <NavLink to="/favorites">Favorites</NavLink>
+                </ul>
+            </div>
+      </nav>
+    )
+}
+
+export default Navbar
