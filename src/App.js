c9d9cf4330dee58b9936e5f4e6e5a321c7513a0a
Emre <emrekurtt1655@gmail.com>
2021-07-26 00:53:40 +0200

    feat: note edit page added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/layout/Navbar'
 import Home from './components/home/Home'
 import Favorite from './components/notes/Favorite';
 import NotesDetail from './components/notes/NotesDetail';
+import EditForm from './components/notes/EditForm';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route path="/favorites" component={Favorite} />
         <Route path="/detail/:id" component={NotesDetail} />
+        <Route path="/edit_note/:id" component={EditForm} />
       </Switch>
     </Router>
   );
